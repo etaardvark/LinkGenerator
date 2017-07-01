d1778a7d159f562ecae9d101c8bd1d098115239f
Peter Marshall <Peter Marshall>
2017-07-01 10:58:25 +0100

    Add project files.

---
+++ LinkGenerator/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace LinkGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearchPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlTop.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.Controls.Add(this.button1);
+            this.pnlTop.Controls.Add(this.btnBrowse);
+            this.pnlTop.Controls.Add(this.label1);
+            this.pnlTop.Controls.Add(this.txtSearchPath);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(1068, 95);
+            this.pnlTop.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.AutoEllipsis = true;
+            this.btnBrowse.Location = new System.Drawing.Point(972, 39);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(84, 23);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Base Directory";
+            // 
+            // txtSearchPath
+            // 
+            this.txtSearchPath.Location = new System.Drawing.Point(94, 39);
+            this.txtSearchPath.Name = "txtSearchPath";
+            this.txtSearchPath.Size = new System.Drawing.Size(872, 20);
+            this.txtSearchPath.TabIndex = 3;
+            this.txtSearchPath.Text = "\\\\PM-QNAP\\MEDIA\\Models";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate URLS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1068, 328);
+            this.panel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1068, 328);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 423);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlTop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearchPath;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ LinkGenerator/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace LinkGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.CheckPathExists = true;
+            fileDialog.Title = "Select Path to Generate URLS";
+            fileDialog.InitialDirectory = txtSearchPath.Text;
+            fileDialog.Multiselect = false;
+            DialogResult res = fileDialog.ShowDialog();
+            if(res == DialogResult.OK)
+            {
+                txtSearchPath.Text = System.IO.Path.GetDirectoryName(fileDialog.FileName);
+            }
+
+            fileDialog.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo info = new DirectoryInfo(txtSearchPath.Text);
+            FileInfo[] files = info.GetFiles("*.jpg", SearchOption.TopDirectoryOnly);
+            List<FileUrl> infoFiles = new List<FileUrl>();
+            foreach(FileInfo f in files)
+            {
+                FileUrl fu = new FileUrl();
+                fu.FileName = f.FullName;
+                fu.URL = f.FullName;
+
+                fu.URL = Regex.Replace(fu.URL,@"\\\\PM-QNAP\\Media\\Models\\", @"",RegexOptions.IgnoreCase);
+                fu.URL = @"http://shdgfx.duckdns.org/" + HttpUtility.HtmlEncode(fu.URL);
+                
+
+                infoFiles.Add(fu);
+
+            }
+
+            dataGridView1.DataSource = infoFiles;
+    
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string rowData = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+            Clipboard.SetText(rowData);
+        }
+    }
+}
